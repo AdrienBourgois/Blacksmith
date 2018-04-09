547bf457700fc033a0e16d2a0f3b4f2a5cb75444
Antonio <a.cantarella@student.isartdigital.com>
2018-04-09 19:34:41 +0200

    Added CameraController. [WIP] Added function to handle zoom out

---
+++ Assets/Game/Scripts/Camera/CameraController.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject _player1;
+    [SerializeField]
+    private GameObject _player2;
+    [SerializeField]
+    private float _maxZoomInDistance;
+    [SerializeField]
+    private float _attenuationFactor;
+
+    void Start () {
+		
+	}
+	
+	void Update ()
+	{
+	    float distance = ComputeDistance();
+        print("distance : " + distance);
+
+	    if (!AreCloseEnough(distance))
+            Zoom(distance);
+	}
+
+
+
+    float ComputeDistance()
+    {
+        Vector3 direction = _player1.transform.position - _player2.transform.position;
+        return Mathf.Abs(direction.x);
+    }
+
+    void Zoom(float distance)
+    {
+        float zoom = distance / _attenuationFactor;
+        this.GetComponent<Camera>().orthographicSize = zoom;
+    }
+
+    bool AreCloseEnough(float distance)
+    {
+
+        return distance <= _maxZoomInDistance;
+    }
+}
