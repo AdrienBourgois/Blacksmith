250825c579d793ea53186465d401485f1da28c95
Maxime <m.wackernie@student.isartdigital.com>
2018-04-22 18:36:06 +0200

    Game Instance unity Events, project version updated

--- Assets/Game/Scripts/GameState.cs
+++ Assets/Game/Scripts/GameState.cs
@@ -6,13 +6,29 @@ public class GameState : MonoBehaviour
 {
 
     private bool isTwoPlayer;
-	// Use this for initialization
-	void Start () {
+    private int currentLevel;
+
+    private enum EGameState
+    {
+        PLAY,
+        PAUSE
+    }
+
+    private enum EGamePlayState
+    {
+        COMBAT,
+        CINEMATIC
+    }
+
+    // Use this for initialization
+    void Start ()
+    {
 		
 	}
 	
 	// Update is called once per frame
-	void Update () {
+	void Update ()
+	{
 		
 	}
 }
