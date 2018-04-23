0b1720f54f1a9999c916d9bbca7111f844188052
Antonio <a.cantarella@student.isartdigital.com>
2018-04-23 13:01:22 +0200

    Added border collider plus his script

---
+++ Assets/Game/Scripts/Camera/CameraBorder.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Game.Scripts.Camera;
+using UnityEngine;
+
+public enum EBorderSide
+{
+    RIGHT = 0,
+    LEFT = 1
+};
+
+public class CameraBorder : MonoBehaviour
+{
+    [SerializeField]
+    private EBorderSide side;
+    private BoxCollider2D collider;
+    private CameraController cameraController;
+
+    public delegate float
+    private System.Action positionFunction;
+
+    private void Start()
+    {
+        cameraController = GetComponentInParent<CameraController>();
+        collider = this.gameObject.GetComponent<BoxCollider2D>();
+
+        if (side == EBorderSide.RIGHT)
+            positionFunction = ComputeRightPosition;
+        else
+            positionFunction = ComputeLeftPosition;
+    }       
+
+	void Update ()
+	{
+	    positionFunction();
+	}
+
+    private void ComputeRightPosition()
+    {
+        Vector3 position = this.transform.parent.position;
+        position.x += cameraController.HalfHorizontalViewingVolume;
+        this.transform.position = position;
+        Vector2 collider_size = collider.size;
+        collider_size.y = cameraController.VerticalViewingVolume;
+        collider.size = collider_size;
+    }
+
+    private void ComputeLeftPosition()
+    {
+        Vector3 position = this.transform.parent.position;
+        position.x -= cameraController.HalfHorizontalViewingVolume;
+        this.transform.position = position;
+        Vector2 collider_size = collider.size;
+        collider_size.y = cameraController.VerticalViewingVolume;
+        collider.size = collider_size;
+    }
+}
--- Assets/Game/Scripts/Camera/CameraController.cs
+++ Assets/Game/Scripts/Camera/CameraController.cs
@@ -1,97 +1,103 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
-public class CameraController : MonoBehaviour
+namespace Game.Scripts.Camera
 {
-    [SerializeField]
-    private GameObject _player1;
-    [SerializeField]
-    private GameObject _player2;
-    [SerializeField]
-    private float _maxZoomInDistance;
-    [SerializeField]
-    private float _maxZoomOutDistance;
-    [SerializeField]
-    private float _attenuationFactor;
-    [SerializeField]
-    [Range(0f, 1f)]
-    private float _smoothnes;
-
-    private Camera _gameCamera;
-
-    private float AspectRatio
+    public class CameraController : MonoBehaviour
     {
-        get { return (float)(_gameCamera.pixelWidth) / (float)(_gameCamera.pixelHeight); }
-    }
+        [SerializeField]
+        private GameObject player1;
+        [SerializeField]
+        private GameObject player2;
+        [SerializeField]
+        private float maxZoomInDistance;
+        [SerializeField]
+        private float maxZoomOutDistance;
+        [SerializeField]
+        private float attenuationFactor;
+        [SerializeField]
+        [Range(0f, 1f)]
+        private float smoothness;
 
-    private float VerticalViewingVolume
-    {
-        get { return _gameCamera.orthographicSize * 2f; }
-    }
+        private UnityEngine.Camera gameCamera;
 
-    private float HorizontalViewingVolume
-    {
-        get { return VerticalViewingVolume * AspectRatio; }
-    }
+        private float AspectRatio
+        {
+            get { return (float)(gameCamera.pixelWidth) / (float)(gameCamera.pixelHeight); }
+        }
 
-    void Start ()
-    {
-        _gameCamera = this.GetComponent<Camera>();
-    }
+        public float VerticalViewingVolume
+        {
+            get { return gameCamera.orthographicSize * 2f; }
+        }
+
+        private float HorizontalViewingVolume
+        {
+            get { return VerticalViewingVolume * AspectRatio; }
+        }
+
+        public float HalfHorizontalViewingVolume
+        {
+            get { return HorizontalViewingVolume / 2f; }
+        }
+
+        void Start ()
+        {
+            gameCamera = this.GetComponent<UnityEngine.Camera>();
+        }
 	
-	void Update ()
-	{
-	    float distance = ComputeDistance();
+        void Update ()
+        {
+            float distance = ComputeDistance();
 
-        ComputeZoom(distance);
-        //else
-        //    Zoom(_maxZoomInDistance);
-        //print("pixelWidth " + _gameCamera.pixelWidth);
-	    //print("scaledPixelWidth " + _gameCamera.scaledPixelWidth);
-        //print("rect.x " + _gameCamera.rect.x);
-        //print("rect.y " + _gameCamera.rect.y);
-        //print("rect.size " + _gameCamera.rect.size);
-        //print("rect.width " + _gameCamera.rect.width);
-        //print("rect.heigt " + _gameCamera.rect.height);
-        //print("pixelRect.x " + _gameCamera.pixelRect.x);
-        //   print("pixelRect.y " + _gameCamera.pixelRect.y);
-        //   print("pixelRect.size " + _gameCamera.pixelRect.size);
-        //   print("pixelRect.width " + _gameCamera.pixelRect.width);
-        //   print("pixelRect.heigt " + _gameCamera.pixelRect.height);
-        print("HorizontalViewingVolume = " + HorizontalViewingVolume / 2f);
-    }
+            ComputeZoom(distance);
+            //else
+            //    Zoom(_maxZoomInDistance);
+            //print("pixelWidth " + _gameCamera.pixelWidth);
+            //print("scaledPixelWidth " + _gameCamera.scaledPixelWidth);
+            //print("rect.x " + _gameCamera.rect.x);
+            //print("rect.y " + _gameCamera.rect.y);
+            //print("rect.size " + _gameCamera.rect.size);
+            //print("rect.width " + _gameCamera.rect.width);
+            //print("rect.heigt " + _gameCamera.rect.height);
+            //print("pixelRect.x " + _gameCamera.pixelRect.x);
+            //   print("pixelRect.y " + _gameCamera.pixelRect.y);
+            //   print("pixelRect.size " + _gameCamera.pixelRect.size);
+            //   print("pixelRect.width " + _gameCamera.pixelRect.width);
+            //   print("pixelRect.heigt " + _gameCamera.pixelRect.height);
+            print("HorizontalViewingVolume = " + HorizontalViewingVolume / 2f);
+        }
 
-    float ComputeDistance()
-    {
-        Vector3 direction = _player1.transform.position - _player2.transform.position;
-        return Mathf.Abs(direction.x);
-    }
+        float ComputeDistance()
+        {
+            Vector3 direction = player1.transform.position - player2.transform.position;
+            return Mathf.Abs(direction.x);
+        }
 
-    void ComputeZoom(float distance)
-    {
-        float raw_zoom = distance / _attenuationFactor;
-        float smooth_zoom = Mathf.Lerp(_gameCamera.orthographicSize, raw_zoom, _smoothnes);
-       // print("smooth_zoom : " + smooth_zoom);
+        void ComputeZoom(float _distance)
+        {
+            float raw_zoom = _distance / attenuationFactor;
+            float smooth_zoom = Mathf.Lerp(gameCamera.orthographicSize, raw_zoom, smoothness);
+            // print("smooth_zoom : " + smooth_zoom);
 
-        if (MaxZoomInReached(smooth_zoom) || MaxZoomOutReached(smooth_zoom))
-            return;
+            if (MaxZoomInReached(smooth_zoom) || MaxZoomOutReached(smooth_zoom))
+                return;
 
-        _gameCamera.orthographicSize = smooth_zoom;
-    }
+            gameCamera.orthographicSize = smooth_zoom;
+        }
 
-    bool AreCloseEnough(float distance)
-    {
-        return distance <= _maxZoomInDistance;
-    }
+        bool AreCloseEnough(float _distance)
+        {
+            return _distance <= maxZoomInDistance;
+        }
 
-    bool MaxZoomInReached(float value)
-    {
-        return value <= _maxZoomInDistance;
-    }
+        bool MaxZoomInReached(float _value)
+        {
+            return _value <= maxZoomInDistance;
+        }
 
-    bool MaxZoomOutReached(float value)
-    {
-        return value >= _maxZoomOutDistance;
+        bool MaxZoomOutReached(float _value)
+        {
+            return _value >= maxZoomOutDistance;
+        }
     }
 }
