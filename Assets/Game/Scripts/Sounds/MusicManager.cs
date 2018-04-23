3fdf6afdd3e2cd07c149afe3a4a9d398f16c7abe
AdrienBourgois <a.bourgois@student.isartdigital.com>
2018-04-23 12:03:41 +0200

    Basic Music manager

---
+++ Assets/Game/Scripts/Sounds/MusicManager.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+
+namespace Game.Scripts.Sounds
+{
+    public class MusicManager : MonoBehaviour
+    {
+        [FMODUnity.EventRef]
+        [SerializeField]
+        private string musicEvent;
+
+        private FMOD.Studio.EventInstance musicInstance;
+
+        private void Start()
+        {
+            musicInstance = FMODUnity.RuntimeManager.CreateInstance(musicEvent);
+            musicInstance.start();
+        }
+
+        private void OnDestroy()
+        {
+            musicInstance.release();
+        }
+    }
+}
