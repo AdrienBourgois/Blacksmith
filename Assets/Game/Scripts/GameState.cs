818fea97cd6497ffa6faa66b1b558e50f62b022e
Antonio <a.cantarella@student.isartdigital.com>
2018-04-25 11:45:13 +0200

    Added Callbacks To GameState

--- Assets/Game/Scripts/GameState.cs
+++ Assets/Game/Scripts/GameState.cs
@@ -18,14 +18,20 @@ namespace Game.Scripts
         private EGameState eGameState = EGameState.MAIN_MENU;
         private EGamePlayState eGamePlayState = EGamePlayState.EXPLORATION;
 
-        private enum EGameState
+        public delegate void GameStateDelegate(EGameState _e_game_state);
+        public delegate void GamePlayStateDelegate(EGamePlayState _e_game_play_state);
+
+        private GameStateDelegate gameStateCallback;
+        private GamePlayStateDelegate gamePlayStateCallback;
+
+        public enum EGameState
         {
             MAIN_MENU,
             IN_GAME,
             PAUSED
         }
 
-        private enum EGamePlayState
+        public enum EGamePlayState
         {
             COMBAT,
             EXPLORATION,
@@ -42,22 +48,57 @@ namespace Game.Scripts
         private void Start()
         {
             DontDestroyOnLoad(this);
-            FindObjectOfType<CombatZoneTrigger>().SubscribeToEnterCombatZoneCallback(SwitchToCombatState);
+            //FindObjectOfType<CombatZoneTrigger>().SubscribeToEnterCombatZoneCallback(SwitchToCombatState);
         }
 
         #endregion
 
-        private void SwitchToCombatState()
+        #region CallbackSubscription
+        public void SubscribeToGameStateCallback(GameStateDelegate _function_pointer)
+        {
+            gameStateCallback += _function_pointer;
+        }
+
+        public void SubscribeToGamePlayStateCallback(GamePlayStateDelegate _function_pointer)
+        {
+            gamePlayStateCallback += _function_pointer;
+        }
+        #endregion
+
+        #region CallbackUnsubscription
+        public void UnsubscribeToGameStateCallback(GameStateDelegate _function_pointer)
+        {
+            gameStateCallback -= _function_pointer;
+        }
+
+        public void UnsubscribeToGamePlayStateCallback(GamePlayStateDelegate _function_pointer)
+        {
+            gamePlayStateCallback -= _function_pointer;
+        }
+        #endregion
+
+        private void SwitchToCombatGamePlayState()
         {
             print("SwitchToCombatState");
+            SwitchGamePlayState(EGamePlayState.COMBAT);
         }
 
         private void SwitchGameState(EGameState _new_e_game_state)
         {
-            eGameState = _new_e_game_state; 
+            eGameState = _new_e_game_state;
 
-            // invoke event with state ?
+            if (gameStateCallback != null)
+                gameStateCallback(eGameState);
         }
+
+        private void SwitchGamePlayState(EGamePlayState _new_e_game_play_state)
+        {
+            eGamePlayState = _new_e_game_play_state;
+
+            if (gamePlayStateCallback != null)
+                gamePlayStateCallback(eGamePlayState);
+        }
+
         public void SetIsTwoPlayers(bool _two_player)
         {
             isTwoPlayer = _two_player;
