6954e3e58ff2cbcaef401c2e632d604afdd05211
Antonio <a.cantarella@student.isartdigital.com>
2018-05-13 14:15:07 +0200

    Started combat system. Added SoCombo and ACommand.

---
+++ Assets/Game/Scripts/ComboSystem/ACommand.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+
+namespace Game.Scripts.ComboSystem
+{
+    [System.Serializable]
+    public abstract class ACommand : ScriptableObject
+    {
+        public delegate void CommandDelegate();
+
+        private CommandDelegate commandFunction;
+
+        public void SubscribeToCommandFunction(CommandDelegate _function_pointer)
+        {
+            commandFunction += _function_pointer;
+        }
+
+        public void UnsubscribeToCommandFunction(CommandDelegate _function_pointer)
+        {
+            if (commandFunction != null)
+                commandFunction -= _function_pointer;
+        }
+
+        protected abstract void Execute();
+    }
+}
---
+++ Assets/Game/Scripts/ComboSystem/SOCombo.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Game.Scripts.ComboSystem;
+using Game.Scripts.Timer;
+using UnityEngine;
+
+//[System.Serializable]
+//public class ComboData<TYPE1, TYPE2>
+//{
+//    public TYPE2 command;
+//    public TYPE1 value;
+//}
+
+[System.Serializable]
+public class ComboData
+{
+    public ACommand command;
+    public float timeOut;
+}
+
+
+public class SOCombo : MonoBehaviour
+{
+    //[SerializeField] private ACommand[] commandArray;
+
+    //[System.Serializable] public class FArray : ComboData<float, ACommand> { }
+
+    [SerializeField] private ComboData[] comboArray;
+
+    private int[] timerIdArray;
+    //[SerializeField] private Dictionary<int, float> dict;
+
+	void Start () {
+        timerIdArray = new int[comboArray.Length];
+
+	    for (int i = 0; i < comboArray.Length; ++i)
+	    {
+	        timerIdArray[i] = TimerManager.Instance.AddTimer("Combo", comboArray[i].timeOut, false, false, OnTimeExpired);
+        }
+	}
+	
+	void Update () {
+		
+	}
+
+    private void OnTimeExpired()
+    {
+        print("Combo Failed");
+    }
+}
