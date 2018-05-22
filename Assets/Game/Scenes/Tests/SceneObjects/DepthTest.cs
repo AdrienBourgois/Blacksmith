63243d43bbb0208764c9a9cbc8e14ae2c499bde0
AdrienBourgois <a.bourgois@student.isartdigital.com>
2018-05-22 23:43:25 +0200

    Add method for depth test

---
+++ Assets/Game/Scenes/Tests/SceneObjects/DepthTest.cs
@@ -0,0 +1,17 @@
+﻿using Game.Scripts.SceneObjects;
+using UnityEngine;
+
+public class DepthTest : SceneObject
+{
+    public SceneObject scene;
+
+    public bool isOverlap = false;
+
+    public float depth = 1f;
+
+    protected override void Update()
+    {
+        SceneObject[] objects_in_layer = GetObjectsInLayer(depth, LayerMask.GetMask("Collectible"));
+        isOverlap = objects_in_layer != null && objects_in_layer.Length > 0;
+    }
+}
--- Assets/Game/Scripts/SceneObjects/SceneObject.cs
+++ Assets/Game/Scripts/SceneObjects/SceneObject.cs
@@ -1,4 +1,6 @@
-﻿using UnityEngine;
+﻿using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
 
 namespace Game.Scripts.SceneObjects
 {
@@ -57,6 +59,43 @@ namespace Game.Scripts.SceneObjects
         protected bool IsInCameraSpace(Vector3 _game_point)
         {
             return GamePhysic.IsPointInCollider(_game_point, "Camera");
+        }
+
+        public bool DoesPointOverlapDepth(Vector3 _point, float _depth)
+        {
+            Vector3 unity_point = _point.ToUnitySpace();
+
+            SpriteRenderer sprite_renderer = GetComponent<SpriteRenderer>();
+            if (sprite_renderer)
+            {
+                if (sprite_renderer.bounds.Contains(unity_point))
+                    return Mathf.Abs(_point.z - location.z) < _depth;
+            }
+
+            Collider2D collider2_d = GetComponent<Collider2D>();
+            if (collider2_d)
+            {
+                if (collider2_d.OverlapPoint(unity_point))
+                    return Mathf.Abs(_point.z - location.z) < _depth;
+            }
+            return false;
+        }
+
+        public SceneObject[] GetObjectsInLayer(float _depth, LayerMask _mask)
+        {
+            Collider2D[] colliders = Physics2D.OverlapPointAll((location + Vector3.forward * (_depth)).ToUnitySpace(), _mask);
+            Collider2D[] colliders_up = Physics2D.OverlapPointAll((location + Vector3.forward * (_depth / 2f)).ToUnitySpace(), _mask);
+            colliders = colliders.Concat(colliders_up).ToArray();
+            colliders = colliders.Distinct().ToArray();
+            if (colliders.Length > 0)
+                return colliders.Select(_col => _col.GetComponent<SceneObject>()).Where(_scene_object => Mathf.Abs(_scene_object.location.z - location.z) < _depth).ToArray();
+
+            return null;
+        }
+
+        public bool IsInRadius(Vector3 _point, float _radius)
+        {
+            return (location - _point).magnitude < _radius;
         }
 
         protected virtual void UpdateRuntimeDebug()
