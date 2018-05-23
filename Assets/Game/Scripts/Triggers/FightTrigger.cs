5051693c0f3374dad297e8634b8b33ef1532a88e
Antonio <a.cantarella@student.isartdigital.com>
2018-05-23 11:48:43 +0200

    Trigger ready

--- Assets/Game/Scripts/Triggers/FightTrigger.cs
+++ Assets/Game/Scripts/Triggers/FightTrigger.cs
@@ -1,5 +1,9 @@
 ﻿using System;
+using Game.Scripts.Entity;
+using UnityEngine;
 using Game.Scripts.Interfaces;
+using Game.Scripts.SceneObjects;
+using UnityEngine.Assertions;
 
 namespace Game.Scripts.Triggers
 {
@@ -15,27 +19,28 @@ namespace Game.Scripts.Triggers
 
         public override void Trigger()
         {
-            //this.gameObject.SetActive(false);
-            //foreach (Wave wave in waves)
-            //{
-            //    EnemySpawn[] spawn_array = wave.spawns;
-            //    foreach (EnemySpawn enemy_spawn in spawn_array)
-            //    {
-            //        if (enemy_spawn.type == EnemyType.NORMAL)
-            //        {
-            //            EnemyEntity enemy = Instantiate(EntityManager.instance.meleeEnemyPrefab, Vector3.zero, Quaternion.identity).GetComponent<EnemyEntity>();
-            //            enemy.location = enemy_spawn.location;
-            //            enemy.ToFloor();
-            //        }
-            //        else if (enemy_spawn.type == EnemyType.WEAK)
-            //        {
-            //            EnemyEntity enemy = Instantiate(EntityManager.instance.rangeEnemyPrefab, Vector3.zero, Quaternion.identity).GetComponent<EnemyEntity>();
-            //            enemy.location = enemy_spawn.location;
-            //            enemy.ToFloor();
-            //        }
-            //    }
-            //}
-            //Debug.Log("END LOOP");
+            foreach (Wave wave in waves)
+            {
+                EnemySpawn[] spawn_array = wave.spawns;
+                foreach (EnemySpawn enemy_spawn in spawn_array)
+                {
+                    if (enemy_spawn.type == EnemyType.NORMAL)
+                    {
+                        SceneObjects.SceneObject enemy = Instantiate(EntityManager.instance.meleeEnemyPrefab, Vector3.zero, Quaternion.identity).GetComponent<SceneObjects.SceneObject>();
+                        Assert.IsNotNull(enemy, "[FightTrigger.Trigger()] Error : the variable 'enemy' is NULL");
+                        enemy.location = enemy_spawn.location;
+                        enemy.ToFloor();
+                    }
+                    else if (enemy_spawn.type == EnemyType.WEAK)
+                    {
+                        SceneObjects.SceneObject enemy = Instantiate(EntityManager.instance.rangeEnemyPrefab, Vector3.zero, Quaternion.identity).GetComponent<SceneObjects.SceneObject>();
+                        Assert.IsNotNull(enemy, "[FightTrigger.Trigger()] Error : the variable 'enemy' is NULL");
+                        enemy.location = enemy_spawn.location;
+                        enemy.ToFloor();
+                    }
+                }
+            }
+            Debug.Log("END LOOP");
         }
     }
 }
